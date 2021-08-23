25e86b1f87c203f781215522acf6ed3a05ca185a
Sam <40672605+parmsam@users.noreply.github.com>
2021-08-22 23:35:03 -0400

    Update rcpp-learning.R

--- rcpp-learning.R
+++ rcpp-learning.R
@@ -1,35 +1,56 @@
-# Load libraries
+# load libraries ----
 library(Rcpp)
 library(microbenchmark)
 
-# Define the function sum_loop
+# define the function sum_loop ----
 sum_loop <- function(x) {
   result <- 0
   for(i in x){result = i + result}
   result
 }
 
-# Check for equality 
+# check for equality ----
 all.equal(sum_loop(x), sum(x))
 
-# Compare the performance
+# compare the performance ----
 microbenchmark(sum_loop = sum_loop(x), R_sum = sum(x))
 
-# Evaluate Rcpp 
+# evaluate Rcpp ----
 evalCpp("40+2")
 evalCpp("17/2")
 
-# Casting 
+# casting ----
 evalCpp("(double)17/2")
 evalCpp("(int)7.25")
 evalCpp("(int)7.95")
 
-#assignment 
+# assignment  ----
 x <- evalCpp('2+2')
-
-# Evaluate 2 + 2 in R
 y <- 2+2
 
-# Storage modes of x and y
+# storage modes ----
 storage.mode(x)
 storage.mode(y)
+
+#declare functions ----
+cppFunction("
+  int fun(){
+    int x= 37;
+    return x;
+  }
+")
+fun()
+
+cppFunction('
+double add(double x, double y){
+  double z = x + y;
+  return z;
+}')
+add(2,3)
+
+cppFunction('
+double subtract(double x, double y){
+  Rprintf("some message here\\n");
+  return x - y;
+}')
+subtract(2.5,3)
